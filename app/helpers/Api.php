b0da5f2c4af472ab73fbd870a773ab95fcec1e22
quangpt-TypeScript <quangpt@ominext.com>
2019-11-04 20:41:22 +0700

    first commit

---
+++ app/helpers/Api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 11/1/2019
+ * Time: 3:33 PM
+ */
+namespace App\Helpers;
+
+class Api {
+    public static function r_response($data = [], $message = null, $code = 'S001', $status = 200) {
+        return response()->json(['body' => $data, 'msg' => $message, 'code' => $code], $status,
+            ['Content-type'=> 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+}
